a01aa9dcfb0908aa9e124c79ed6fc94af08a0dca
marco <marco.crucitti96@gmail.com>
2023-11-17 12:57:26 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app')
 <body>
     @section('content')
-    <div class="container">
+    <div class="container pt-5">
         @if (Route::has('login'))
-            <div class="flex2">
-                @auth
-                    <a href="{{ url('/home') }}" class="btn btn-primary">Home</a>
-                @else
-                    <a href="{{ route('login') }}" class="btn btn-primary">Log in</a>
+        <div class="flex2">
+            @auth
+            <a href="{{ url('/home') }}" class="btn btn-primary">Home</a>
+            @else
+                <a href="{{ route('login') }}" class="btn btn-primary">Log in</a>
 
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="btn btn-primary">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="btn btn-primary">Register</a>
+                @endif
+            @endauth
+        </div>
+        @endif  
     </div>
     @endsection  
 </body>
