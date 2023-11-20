7d1334188ab37dc30f445c80a92ff16bffffda65
marco <marco.crucitti96@gmail.com>
2023-11-20 12:58:27 +0100

    modifiche recenti

--- app/Http/Controllers/AutoController.php
+++ app/Http/Controllers/AutoController.php
@@ -10,16 +10,56 @@ class AutoController extends Controller
 {
     public function create($clienteId)
     {
+        $auto = [new Auto()];
         $cliente = Cliente::find($clienteId);
     
         if (!$cliente) {
             // Gestisci il caso in cui il cliente non viene trovato
         }
     
-        return view('crea_auto', compact('cliente'));
+        return view('crea_auto', compact('cliente', 'auto'));
     }
+
+
     
 
+// public function store(Request $request, $clienteId)
+// {
+//     $request->validate([
+//         'modello' => 'required',
+//         'targa' => 'required',
+//         'n_telaio' => 'required',
+//         'marca' => 'required',
+//         'anno' => 'required',
+//         'chilometri' => 'required',
+//         'note_stato' => 'required',
+//         'data_intervento' => 'required',
+//     ]);
+
+//     // Trova il cliente
+//     $cliente = Cliente::find($clienteId);
+
+//     if (!$cliente) {
+//         return redirect()->route('clienti.index')->with('error', 'Cliente non trovato');
+//     }
+
+//     // Crea un nuovo oggetto Auto
+//     $auto = new Auto([
+//         'modello' => $request->input('modello'),
+//         'targa' => $request->input('targa'),
+//         'n_telaio' => $request->input('n_telaio'),
+//         'marca' => $request->input('marca'),
+//         'anno' => $request->input('anno'),
+//         'chilometri' => $request->input('chilometri'),
+//         'note_stato' => $request->input('note_stato'),
+//         'data_intervento' => $request->input('data_intervento'),
+//     ]);
+
+//     // Associa l'auto al cliente
+//     $cliente->auto()->save($auto);
+
+//     return redirect()->route('clienti.index')->with('success', 'Auto aggiunta con successo al cliente');
+// }
 public function store(Request $request, $clienteId)
 {
     $request->validate([
@@ -33,30 +73,32 @@ public function store(Request $request, $clienteId)
         'data_intervento' => 'required',
     ]);
 
-    // Trova il cliente
-    $cliente = Cliente::find($clienteId);
+    $autosData = $request->input('auto');
 
-    if (!$cliente) {
-        return redirect()->route('clienti.index')->with('error', 'Cliente non trovato');
+    foreach ($autosData as $autoData) {
+        $auto = new Auto([
+            'modello' => $autoData['modello'],
+            'targa' => $autoData['targa'],
+            'n_telaio' => $autoData['n_telaio'],
+            'marca' => $autoData['marca'],
+            'anno' => $autoData['anno'],
+            'chilometri' => $autoData['chilometri'],
+            'note_stato' => $autoData['note_stato'],
+            'data_intervento' => $autoData['data_intervento'],
+        ]);
+
+        $cliente = Cliente::find($clienteId);
+
+        if (!$cliente) {
+            return redirect()->route('cliente.create')->with('error', 'Cliente non trovato');
+        }
+
+        $cliente->auto()->save($auto);
     }
 
-    // Crea un nuovo oggetto Auto
-    $auto = new Auto([
-        'modello' => $request->input('modello'),
-        'targa' => $request->input('targa'),
-        'n_telaio' => $request->input('n_telaio'),
-        'marca' => $request->input('marca'),
-        'anno' => $request->input('anno'),
-        'chilometri' => $request->input('chilometri'),
-        'note_stato' => $request->input('note_stato'),
-        'data_intervento' => $request->input('data_intervento'),
-    ]);
-
-    // Associa l'auto al cliente
-    $cliente->auto()->save($auto);
-
-    return redirect()->route('clienti.index')->with('success', 'Auto aggiunta con successo al cliente');
+    return redirect()->route('clienti.index')->with('success', 'Auto aggiunte con successo al cliente');
 }
+
 
 
     
--- resources/views/crea_auto.blade.php
+++ resources/views/crea_auto.blade.php
@@ -6,7 +6,41 @@
     <div class="container">
         <form action="{{ route('clienti.auto.store', $cliente->id) }}" method="POST" class="row" id="autoForm">
             @csrf
-            <h3 class="titolo">Dati Auto</h3>
+            @foreach ($auto as $index => $autoData)
+                <div class="col-md-6">
+                    <label class="form-label">Modello</label>
+                    <input type="text" name="auto[0][modello]" class="form-control">
+                </div>
+                <div class="col-md-6">
+                    <label class="form-label">Targa</label>
+                    <input type="text" name="auto[0][targa]" class="form-control">
+                </div>
+                <div class="col-md-6">
+                    <label class="form-label">Numero del telaio</label>
+                    <input type="text" name="auto[0][n_telaio]" class="form-control">
+                </div>
+                <div class="col-md-6">
+                    <label class="form-label">Marca</label>
+                    <input type="text" name="auto[0][marca]" class="form-control">
+                </div>
+                <div class="col-md-6">
+                    <label class="form-label">Anno</label>
+                    <input type="text" name="auto[0][anno]" class="form-control">
+                </div>
+                <div class="col-md-6">
+                    <label class="form-label">Chilometri</label>
+                    <input type="text" name="auto[0][chilometri]" class="form-control">
+                </div>
+                <div class="col-md-6">
+                    <label for="note_stato" class="form-label">Note sulle modifiche / stato dell'auto</label>
+                    <textarea class="form-control" name="auto[0][note_stato]" id="note_stato" rows="1"></textarea>
+                </div>
+                <div class="col-md-6">
+                    <label class="form-label">Data dell'intervento</label>
+                    <input type="text" name="auto[0][data_intervento]" class="form-control">
+                </div>
+            @endforeach
+            {{-- <h3 class="titolo">Dati Auto</h3>
             <div class="col-md-6">
                 <label class="form-label">Modello</label>
                 <input type="text" name="modello" class="form-control">
@@ -36,13 +70,13 @@
                 <textarea class="form-control" name="note_stato" id="note_stato" rows="1"></textarea>
             </div>
             <div class="col-md-6">
-                {{-- <div class="form-group">
+                <div class="form-group">
                     <label class="active form-label" for="dateStandard">Data dell'intervento</label>
                     <input type="date" id="dateStandard" name="data_intervento">
-                </div> --}}
+                </div> 
                 <label class="form-label">Data dell'intervento</label>
                 <input type="text" name="data_intervento" class="form-control">
-            </div>
+            </div> --}}
             <div class="col-md-12 mt-3">
                 <button type="submit" class="btn btn-primary">Salva Auto</button>
             </div>
