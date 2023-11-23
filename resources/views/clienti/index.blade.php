1880588b73bf0e6536f77aee5c3f68c0fdf60e8c
marco <marco.crucitti96@gmail.com>
2023-11-23 12:58:36 +0100

    t

--- resources/views/clienti/index.blade.php
+++ resources/views/clienti/index.blade.php
@@ -86,13 +86,42 @@
                             </a>
                         </td>
                         <td>
-                            <form method="POST" action="{{ route('clienti.destroy', $cliente->id) }}">
+                            <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                            Elimina Cliente
+                        </button>
+                        
+                        <!-- Modal -->
+                        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="staticBackdropLabel">Attenzione!</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                    Sei sicuro di voler eliminare il cliente?
+                                    </div>
+                                    <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                                        <form method="POST" action="{{ route('clienti.destroy', $cliente->id) }}">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="btn btn-danger mt-2" type="submit">
+                                                Elimina Cliente
+                                            </button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                            {{-- <form method="POST" action="{{ route('clienti.destroy', $cliente->id) }}">
                                 @csrf
                                 @method('DELETE')
                                 <button class="btn btn-danger mt-2" type="submit">
                                     Elimina Cliente
                                 </button>
-                            </form>
+                            </form> --}}
                         </td>
                     </tr>
                 @endforeach
