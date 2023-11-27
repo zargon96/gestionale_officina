3ba75b646bf1cc6f0c0646791d06b4e35b0933b6
marco <marco.crucitti96@gmail.com>
2023-11-27 09:25:09 +0100

    t

--- app/Http/Controllers/InterventoController.php
+++ app/Http/Controllers/InterventoController.php
@@ -11,34 +11,43 @@ class InterventoController extends Controller
     {
         $auto = Auto::where('cliente_id', $cliente_id)->with('cliente')->find($auto_id);
 
+        if ($auto) {
+            $cliente = $auto->cliente;
+        } else {
+            return redirect()->route('clienti.index')->with('error', 'Auto non trovata per il cliente');
+        }
+    
+
         return view('interventi.crea_intervento', compact('cliente', 'auto'));
     }
-    public function store(Request $request, $autoId)
+
+    public function store(Request $request, $cliente_id, $auto_id)
     {
         // Trova l'auto tramite l'ID
-        $auto = Auto::find($autoId);
-
+        $auto = Auto::where('cliente_id', $cliente_id)->with('cliente')->find($auto_id);
+    
         if (!$auto) {
             return redirect()->back()->with('error', 'Auto non trovata');
         }
-
+    
         // Validazione della richiesta
         $request->validate([
             'note_stato' => 'required',
-            'data_intervento' => 'required|date',
+            'data_intervento' => 'required',
         ]);
-
+    
         // Creiamo un nuovo intervento
         $intervento = new Intervento([
             'note_stato' => $request->input('note_stato'),
             'data_intervento' => $request->input('data_intervento'),
         ]);
-
+    
         // Associamo l'auto all'intervento
         $auto->interventi()->save($intervento);
-
+    
         return redirect()->back()->with('success', 'Intervento aggiunto con successo');
     }
+    
 }
 
 
